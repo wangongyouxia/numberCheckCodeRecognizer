4858bd9809cf3da5bbf39f61db71403f0fa16e34
wangongyouxia <709865788@qq.com>
2016-08-16 09:44:58 +0800

    Delete TranPic.py

--- TranPic.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image                     
-def ConvertToJpg(inputFileName,outputFileName):
-	im = Image.open(inputFileName)   
-	im = im.convert('RGB') 
-	im.save(outputFileName,"jpeg")  
-
-if __name__ == '__main__':
-	ConvertToJpg('save_get.gif','output.jpg')
