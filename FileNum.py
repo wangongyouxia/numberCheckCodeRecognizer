91dc052c3c648057d4e4bd971ab0110fe006df70
wangongyouxia <709865788@qq.com>
2016-08-16 09:44:31 +0800

    Delete FileNum.py

--- FileNum.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def GetFileNum(path):
-	count = sum([len(x) for _, _, x in os.walk(os.path.dirname(path))])
-	return (count)               #输出结果
-if __name__ == '__main__':
-	print (GetFileNum('./1/'))
-
