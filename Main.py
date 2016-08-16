078e4da816007d90b21e00c5be4050768865ab60
wangongyouxia <709865788@qq.com>
2016-08-16 09:27:03 +0800

    edit Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from PIL import Image
-from random import Random
-import cv2
 
 from GetSingleChar import *
 from Detect import *
 from Recognize import *
-from TranPic import *
 from MachineLearn import *
+def Test():
+	learnNum = 20
+	g = Detect('output.jpg','g.jpg')
+	if SplitToSingle(g) != 4:
+		print('不是四个字符！')
+		exit(-1)
+	CheckCode = RecognizeCheckCode(4,learnNum)
+	if CheckCode == '-1':
+		print('识别失败')
+		exit(-1)
+	Learn(20)
+	print (CheckCode)
 
-learnNum = 20
-g = Detect('output.jpg','g.jpg')
-if SplitToSingle(g) != 4:
-	print ('不是四个字符！')
-	exit(-1)
-CheckCode = RecognizeCheckCode(4,learnNum)
-print (CheckCode)
+if __name__ == '__main__':
+	Test()
